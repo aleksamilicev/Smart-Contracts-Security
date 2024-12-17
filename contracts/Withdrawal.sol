eb6e88b3dc43a8d7f66c842cfb55ead0c71e2c93
aleksamilicev <amilicev92@gmail.com>
2024-12-17 15:00:57 +0100

    Implementacija Withdrawal ugovora

--- contracts/Withdrawal.sol
+++ contracts/Withdrawal.sol
@@ -2,13 +2,28 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Withdrawal{
-    uint256 public ballance;
 
-    function deposit() public{
-
+    function CheckBallance() public view returns(uint256) {
+        return address(this).balance;   // prikazuje saldo ugovora u Wei
+        // address(this), vraca adresu trenutnog korisnika
+        // .balance vraca vrednost u Wei
+        // i ovo je READ-ONLY
     }
 
-    function withdraw(uint256 ) public{
+    function Deposit() public payable {
+        // BITNO - vrednosti unosis preko VALUE POLJA, a onda sa njima rukujes preko funkcija u contract-u
+        // funkcija je payable i omogucava primanje ETH
+        // msg znaci trenutna poruka koju je korisnik uneo
+        // msg.value je u Wei, 1 ETH = 10^18 Wei, znaci msg.value = 1000000...
+        // require je funkcija koja sluzi kao provera
+        require(msg.value > 0, "Number must be greater then 0!");
+    }
 
+    function Withdraw(uint256 money) public{
+        require(CheckBallance() >= money, "Not enough funds in the contract!");
+
+        // Slanje Wei korisniku koji je pozvao funkciju
+        payable (msg.sender).transfer(money);
+        // msg.sender predstavlja adresu korisnika koji poziva funkciju
     }
 }
